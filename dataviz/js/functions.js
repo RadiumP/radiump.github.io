fd50f61b6e7739ddbc53936b8a4c5f82238b475c
Young <chenyan5@asu.edu>
2026-03-25 21:05:17 -0700

    Update dataviz

--- dataviz/js/functions.js
+++ dataviz/js/functions.js
@@ -10,32 +10,21 @@ var num = [];
 var weight = 70;
 var menu ;
 function initialize() {
-         var myStyles =[
-        {
-            featureType: "poi",
-            elementType: "labels",
-            stylers: [
-              { visibility: "off" }
-            ]
-        }
-        ];
         var latitude = 33.4294,
-            longitude = -111.9431
-            radius = 80000, //how is this set up
-            center = new google.maps.LatLng(latitude,longitude),
-        
-       
+            longitude = -111.9431,
+            radius = 80000,
+            center = [latitude, longitude];
 
-            mapOptions = {
-                center: center,
-                zoom: 12,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                scrollwheel: false,
-                styles:myStyles,
-                disableDoubleClickZoom: true
-            };
+        var map = L.map('map-canvas', {
+            center: center,
+            zoom: 12,
+            scrollWheelZoom: false,
+            doubleClickZoom: false
+        });
 
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
      
          
          
@@ -1108,67 +1097,16 @@ function initialize() {
         
 
 
-      $("#r1").click(function(){
-        latLng = new google.maps.LatLng(menuData[0].latitude, menuData[0].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-
-      });
-      $("#r2").click(function(){
-        latLng = new google.maps.LatLng(menuData[1].latitude, menuData[1].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r3").click(function(){
-        latLng = new google.maps.LatLng(menuData[2].latitude, menuData[2].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r4").click(function(){
-        latLng = new google.maps.LatLng(menuData[3].latitude, menuData[3].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r5").click(function(){
-        latLng = new google.maps.LatLng(menuData[4].latitude, menuData[4].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-    $("#r6").click(function(){
-        latLng = new google.maps.LatLng(menuData[5].latitude, menuData[5].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r7").click(function(){
-        latLng = new google.maps.LatLng(menuData[6].latitude, menuData[6].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r8").click(function(){
-        latLng = new google.maps.LatLng(menuData[7].latitude, menuData[7].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r9").click(function(){
-        latLng = new google.maps.LatLng(menuData[8].latitude, menuData[8].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });
-      $("#r10").click(function(){
-        latLng = new google.maps.LatLng(menuData[9].latitude, menuData[9].longitude); 
-        
-        map.panTo(latLng);
-        map.setZoom(15);
-      });    
+      for (var ri = 0; ri < 10; ri++) {
+        (function(idx) {
+          $("#r" + (idx + 1)).click(function(){
+            if (menuData && menuData[idx]) {
+              map.setView([menuData[idx].latitude, menuData[idx].longitude], 15);
+              showDetail(menuData, idx);
+            }
+          });
+        })(ri);
+      }    
 
     }
 
@@ -1208,169 +1146,192 @@ function initialize() {
     }
 
 
-    function setMarkers(center, radius, map,js) {
-       
-        var bounds = new google.maps.LatLngBounds();   
+    function setMarkers(center, radius, map, js) {
+        var bounds = L.latLngBounds();
         passdata = js;
-        //loop between each of the json elements
+
         for (var i = 0, length = js.length; i < length; i++) {
-            data = js[i];
-         
-            latLng = new google.maps.LatLng(data.latitude, data.longitude); 
+            var data = js[i];
+            var latLng = [data.latitude, data.longitude];
 
+            // Determine rank label style
+            var rankClass = 'rank-label';
+            if (i === 0) rankClass += ' gold';
+            else if (i === 1) rankClass += ' silver';
+            else if (i === 2) rankClass += ' bronze';
 
+            var icon = L.divIcon({
+                className: 'leaflet-rank-icon',
+                html: '<div class="' + rankClass + '">' + (i + 1) + '</div>',
+                iconSize: [32, 32],
+                iconAnchor: [16, 16]
+            });
 
-            
-                // Creating a marker and putting it on the map
-                 var marker = new google.maps.Marker({
-                    position: latLng,
-                    map: map,
-                    title: data.name,
-                    zIndex:1,
-                    opacity:0
-                    
+            var marker = L.marker(latLng, { icon: icon, title: data.name }).addTo(map);
+
+            var popupContent = '<div class="map-popup" style="min-width:220px;font-family:Arial,sans-serif;">' +
+                '<strong style="font-size:14px;">' + data.name + '</strong>' +
+                '<div style="font-size:11px;color:#666;margin:2px 0;">' + (data.categories || '') + '</div>' +
+                '<div style="display:flex;gap:6px;justify-content:center;margin:8px 0;">' +
+                  '<div style="text-align:center"><svg id="pop-gauge-c-' + i + '" width="50" height="50"></svg><div style="font-size:9px;">Combined</div></div>' +
+                  '<div style="text-align:center"><svg id="pop-gauge-r-' + i + '" width="50" height="50"></svg><div style="font-size:9px;">Reviews</div></div>' +
+                  '<div style="text-align:center"><svg id="pop-gauge-p-' + i + '" width="50" height="50"></svg><div style="font-size:9px;">Value</div></div>' +
+                '</div>' +
+                '<div style="font-size:11px;">' + (data.full_address || '') + '</div>' +
+                '</div>';
+
+            marker.bindPopup(popupContent, { maxWidth: 280, minWidth: 220 });
+
+            (function(marker, idx, d) {
+                marker.on('click', function() {
+                    map.setView(marker.getLatLng(), 15);
+                    index = idx;
+                    showDetail(js, idx);
                 });
-               
-              
-            
-            marker.infoWindow = new google.maps.InfoWindow();
-            
-            //marker.infoWindow.setContent(data.name);
-            
-           
+                marker.on('popupopen', function() {
+                    renderPopupGauges(d, idx);
+                });
+            })(marker, i, data);
 
-                
-                if(i<3)bounds.extend(marker.position);  
-        // Attaching a click event to the current marker
-            google.maps.event.addListener(marker, "click", (function(marker,i) {
-            
-            return function() {
-                    boxList[i].open(map, this);
-                    map.panTo(this.getPosition());
-                   
-                    map.setZoom(15);
-  
-                    index = i;
-        
-                }
-            })(marker, i));
-           google.maps.event.addListener(map, 'click', function() {
-            boxList[index].close();
-            map.setZoom(12);
-          });
-
-       
-
-           
+            if (i < 3) bounds.extend(latLng);
             markers.push(marker);
-            
-            var myOp = {
-                 content: document.getElementById("info")
-                ,disableAutoPan: true
-                ,maxWidth: 0
-                ,pixelOffset: new google.maps.Size(-200, -400)
-                ,zIndex: 3
-                ,boxStyle: { 
-                  background: "url('tipbox.gif') no-repeat"
-                  ,opacity: 1
-                  
-                 }
-                
-                ,closeBoxURL:""
-                ,infoBoxClearance: new google.maps.Size(1, 1)
-                ,position:latLng
-                ,isHidden: false
-                ,pane: "mapPane"
-                ,enableEventPropagation: false
-
-            };
-            var ib = new InfoBox(myOp);
-            boxList.push(ib);
         }
-          
-        
-        for (var i = 0; i < markers.length; i++) {
-            
-             var frame = [];
-            
-             frame[0] = document.getElementById("gold");
-             frame[1] = document.getElementById("silver");
-             frame[2] = document.getElementById("bronze");
-             var n = i+1;
-             if(i>2){
-              num[i] = document.getElementById("rank"+n);
-              num[i].innerHTML = "<img src='img/number_"+n+".png'>"
-            }
-            //liq[1] = document.getElementById("fillgauge2");
-            //liq[i].style.cssText = "border: 1px solid black; margin-top: 8px; background: yellow; padding: 5px;";
-           //drawInfoBox(js,i);
 
-            var myOptions = {
-                 content: frame[i]
-                ,disableAutoPan: true
-                ,maxWidth: 0
-                ,pixelOffset: new google.maps.Size(-60, -120)
-                ,zIndex: 3
-                ,boxStyle: { 
-                  background: "url('tipbox.gif') no-repeat"
-                  ,opacity: 1
-                  ,width: "100px"
-                 }
-                
-                ,closeBoxURL:""
-                ,infoBoxClearance: new google.maps.Size(1, 1)
-                ,position:latLng
-                ,isHidden: false
-                ,pane: "mapPane"
-                ,enableEventPropagation: false
-            };
-            var newOptions = {
-                 content: num[i]
-                ,disableAutoPan: true
-                ,maxWidth: 0
-                ,pixelOffset: new google.maps.Size(-48, -90)
-                ,zIndex: 2
-                ,boxStyle: { 
-                  background: "url('tipbox.gif') no-repeat"
-                  ,opacity: 1
-                  ,width: "100px"
-                 }
-                
-                ,closeBoxURL:""
-                ,infoBoxClearance: new google.maps.Size(1, 1)
-                ,position:latLng
-                ,isHidden: false
-                ,pane: "mapPane"
-                ,enableEventPropagation: false
-            };
-            if(i<3){
-             markers[i].infobox = new InfoBox(myOptions);
-            //Open box when page is loaded
-             markers[i].infobox.open(map, markers[i]);
-           }
-           else{
-             markers[i].infobox = new InfoBox(newOptions);
-            //Open box when page is loaded
-             markers[i].infobox.open(map, markers[i]);
-           }
+        if (bounds.isValid()) {
+            map.fitBounds(bounds, { padding: [50, 50] });
         }
-          //map.setCenter(latLng);
-        map.fitBounds(bounds);
-
-        
     }
 
-  
- function DeleteMarkers() {
-        //Loop through all the markers and remove
+    function DeleteMarkers() {
         for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-            //markers[i].infobox.close();
+            markers[i].remove();
         }
         markers = [];
-
+        $("#detail-panel").hide();
     }
 
+    function showDetail(data, idx) {
+        var d = data[idx];
+        $("#detail-name").text(d.name);
+        $("#detail-cate").text(d.categories || '');
+        $("#detail-address").text(d.full_address || '');
+        $("#detail-r1").text(d.top_1 || '');
+        $("#detail-r2").text(d.top_2 || '');
+        $("#detail-r3").text(d.top_3 || '');
+
+        // Clear old gauges and redraw
+        $("#gauge-combined").empty();
+        $("#gauge-review").empty();
+        $("#gauge-price").empty();
+
+        if (typeof loadLiquidFillGauge === 'function') {
+            var cfg1 = liquidFillGaugeDefaultSettings();
+            cfg1.circleColor = "#ee6121";
+            cfg1.textColor = "#21b6ca";
+            cfg1.waveTextColor = "#000000";
+            cfg1.waveColor = "#f39468";
+            cfg1.circleThickness = 0.2;
+            cfg1.textVertPosition = 0.2;
+            cfg1.waveAnimateTime = 1000;
+            cfg1.minValue = 4;
+            cfg1.maxValue = 5;
+            cfg1.displayPercent = false;
+            loadLiquidFillGauge("gauge-combined", d.combine_stars, cfg1);
+
+            var cfg2 = liquidFillGaugeDefaultSettings();
+            cfg2.circleColor = "#21b6ca";
+            cfg2.textColor = "#553300";
+            cfg2.waveTextColor = "#2162ca";
+            cfg2.waveColor = "#20d6cc";
+            cfg2.circleThickness = 0.1;
+            cfg2.circleFillGap = 0.2;
+            cfg2.textVertPosition = 0.8;
+            cfg2.waveAnimateTime = 2000;
+            cfg2.waveHeight = 0.3;
+            cfg2.waveCount = 2;
+            cfg2.minValue = 4;
+            cfg2.maxValue = 5;
+            cfg2.displayPercent = false;
+            loadLiquidFillGauge("gauge-review", d.review_stars, cfg2);
+
+            var cfg3 = liquidFillGaugeDefaultSettings();
+            cfg3.circleThickness = 0.15;
+            cfg3.circleColor = "#78c35f";
+            cfg3.textColor = "#555500";
+            cfg3.waveTextColor = "#529b3a";
+            cfg3.waveColor = "#97d184";
+            cfg3.textVertPosition = 0.8;
+            cfg3.waveAnimateTime = 1000;
+            cfg3.waveHeight = 0.05;
+            cfg3.waveAnimate = true;
+            cfg3.waveRise = false;
+            cfg3.waveOffset = 0.25;
+            cfg3.waveCount = 3;
+            cfg3.minValue = 0;
+            cfg3.maxValue = 5;
+            cfg3.displayPercent = false;
+            loadLiquidFillGauge("gauge-price", 5 - d.price, cfg3);
+        }
+
+        $("#detail-panel").show();
+    }
+
+    function renderPopupGauges(d, idx) {
+        if (typeof loadLiquidFillGauge !== 'function') return;
+
+        var cId = "pop-gauge-c-" + idx;
+        var rId = "pop-gauge-r-" + idx;
+        var pId = "pop-gauge-p-" + idx;
+
+        if (!document.getElementById(cId)) return;
+
+        var cfg1 = liquidFillGaugeDefaultSettings();
+        cfg1.circleColor = "#ee6121";
+        cfg1.textColor = "#21b6ca";
+        cfg1.waveTextColor = "#000000";
+        cfg1.waveColor = "#f39468";
+        cfg1.circleThickness = 0.2;
+        cfg1.textVertPosition = 0.2;
+        cfg1.waveAnimateTime = 1000;
+        cfg1.minValue = 4;
+        cfg1.maxValue = 5;
+        cfg1.displayPercent = false;
+        loadLiquidFillGauge(cId, d.combine_stars, cfg1);
+
+        var cfg2 = liquidFillGaugeDefaultSettings();
+        cfg2.circleColor = "#21b6ca";
+        cfg2.textColor = "#553300";
+        cfg2.waveTextColor = "#2162ca";
+        cfg2.waveColor = "#20d6cc";
+        cfg2.circleThickness = 0.1;
+        cfg2.circleFillGap = 0.2;
+        cfg2.textVertPosition = 0.8;
+        cfg2.waveAnimateTime = 2000;
+        cfg2.waveHeight = 0.3;
+        cfg2.waveCount = 2;
+        cfg2.minValue = 4;
+        cfg2.maxValue = 5;
+        cfg2.displayPercent = false;
+        loadLiquidFillGauge(rId, d.review_stars, cfg2);
+
+        var cfg3 = liquidFillGaugeDefaultSettings();
+        cfg3.circleThickness = 0.15;
+        cfg3.circleColor = "#78c35f";
+        cfg3.textColor = "#555500";
+        cfg3.waveTextColor = "#529b3a";
+        cfg3.waveColor = "#97d184";
+        cfg3.textVertPosition = 0.8;
+        cfg3.waveAnimateTime = 1000;
+        cfg3.waveHeight = 0.05;
+        cfg3.waveAnimate = true;
+        cfg3.waveRise = false;
+        cfg3.waveOffset = 0.25;
+        cfg3.waveCount = 3;
+        cfg3.minValue = 0;
+        cfg3.maxValue = 5;
+        cfg3.displayPercent = false;
+        loadLiquidFillGauge(pId, 5 - d.price, cfg3);
+    }
 
 function getWeight(value){
     weight = value;
